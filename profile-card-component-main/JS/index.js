5a3fc994ae2835a09f303f34ff796749116ee6e9
gabydavilaneves@gmail.com <gabydavilaneves@gmail.com>
2022-07-30 12:06:28 -0300

    feat: Salvando o tema no localStorage

--- profile-card-component-main/JS/index.js
+++ profile-card-component-main/JS/index.js
@@ -2,14 +2,19 @@ const inputContainer = document.querySelector('input');
 
 const rootElement = document.documentElement;
 
+window.onload = function () {
+  getThemeFromLocalStorage()
+}
+
+
 const lightMode = {
-  "--dark-cyan": "hsl(185, 75%, 39%)",
+  "--background-color": "hsl(185, 75%, 39%)",
   "--card-background": "#fff",
   "--text-color": "#000000",
 }
 
 const darkMode = {
-  "--dark-cyan": "#1C1C1C",
+  "--background-color": "#1C1C1C",
   "--card-background": "#4F4F4F",
   "--text-color": "#fff",
 }
@@ -27,8 +32,26 @@ function changeTheme(theme) {
   for(let prop in theme) {
     changeProperty(prop, theme[prop])
   }
+  saveThemeLocalStorage(theme)
 }
 
 function changeProperty(property, value) {
   rootElement.style.setProperty(property, value)
+}
+
+function saveThemeLocalStorage(theme) {
+  localStorage.setItem('theme', JSON.stringify(theme))
+}
+
+function getThemeFromLocalStorage() {
+  const theme = JSON.parse(localStorage.getItem('theme'))//JSON.parse, converte para objeto
+  if(isThemeEqual(theme, darkMode)) inputContainer.checked = true
+  changeTheme(theme)
+}
+
+function isThemeEqual (firstTheme, secondTheme) {
+  for(let prop in firstTheme) {
+    if (firstTheme[prop] != secondTheme[prop]) return false 
+  }
+  return true
 }
